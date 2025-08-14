e7b6d009dbc9fd1b2d6daadc922d17b035ec8c2f
Rithick Roshan <rithick1887@gmail.com>
2025-08-14 21:03:49 +0530

    new2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const app = express();
